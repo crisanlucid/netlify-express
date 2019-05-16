7bdf21156a2c7357a7c8dab312a62700e3dffbfb
crisanlucid <crisan.lucid@yahoo.com>
2019-05-16 13:06:59 +0200

    define routers

--- express/routes/resume.js
+++ express/routes/resume.js
@@ -2,7 +2,7 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET categories listing. */
+/* GET resumes listing. */
 
 router.get('/', function(req, res, next) {
   return res.json(['data']);
--- express/server.js
+++ express/server.js
@@ -3,13 +3,24 @@ const express = require('express');
 const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
+;
+const router = express.Router();
 
-var indexRouter = require('./routes/index');
-var resumesRouter = require('./routes/resume');
+router.get('/', function(req, res, next) {
+  res.render('index', {title: '[Express] Server Page'});
+});
+
+router.get('/api/v1/resume', function(req, res, next) {
+  return res.json(['data']);
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
 
 app.use(bodyParser.json());
-
-app.use('/.netlify/functions/server', indexRouter, resumesRouter);  // path must route to lambda
+app.use('/.netlify/functions/server', router);  // path must route to lambda
 
 module.exports = app;
 module.exports.handler = serverless(app);
