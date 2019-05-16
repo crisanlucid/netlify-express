5b13e8a62a4a64eb6dda7c4ca8251250256aa759
crisanlucid <crisan.lucid@yahoo.com>
2019-05-16 14:07:00 +0200

    revert code

--- express/server.js
+++ express/server.js
@@ -3,17 +3,15 @@ const express = require('express');
 const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
-;
-const router = express.Router();
-
 
 const router = express.Router();
 router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.writeHead(200, {'Content-Type': 'text/html'});
   res.write('<h1>Hello from Express.js!</h1>');
   res.end();
-
-router.get('/api/v1/resume', (req, res) => res.json({ route: req.originalUrl }));
+});
+router.get('/another', (req, res) => res.json({route: req.originalUrl}));
+router.post('/', (req, res) => res.json({postBody: req.body}));
 
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router); // path must route to lambda
