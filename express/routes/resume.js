d7acf03beaf908fd9d5c3c67137e3ad8b03a062b
crisanlucid <crisan.lucid@yahoo.com>
2019-05-16 12:49:02 +0200

    create routes for express

---
+++ express/routes/resume.js
@@ -0,0 +1,9 @@
+'use strict';
+var express = require('express');
+var router = express.Router();
+
+/* GET categories listing. */
+
+router.get('/', function(req, res, next) {
+  return res.json(['data']);
+});
