5079074148e9f230525bf61616b25883d369bb9b
crisanlucid <crisan.lucid@yahoo.com>
2019-05-16 14:01:12 +0200

    update API

--- express/server.js
+++ express/server.js
@@ -6,16 +6,22 @@ const bodyParser = require('body-parser');
 ;
 const router = express.Router();
 
-router.get('/', function(req, res, next) {
-  res.render('index', {title: '[Express] Server Page'});
+
+const router = express.Router();
+router.get('/', (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write('<h1>Hello from Express.js!</h1>');
+  res.end();
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
-router.get('/api/v1/resume', function(req, res, next) {
-  return res.json(['data']);
-});
+router.get('/api/v1/resume', (req, res) => res.json({ route: req.originalUrl }));
 
 app.use(bodyParser.json());
-app.use('/.netlify/functions/server', router);  // path must route to lambda
+app.use('/.netlify/functions/server', router); // path must route to lambda
 
 module.exports = app;
 module.exports.handler = serverless(app);
