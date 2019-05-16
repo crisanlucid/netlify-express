2baee48553b238f2ecd9eb14e24baf80f6db0b67
crisanlucid <crisan.lucid@yahoo.com>
2019-05-16 13:02:00 +0200

    clean code

--- express/server.js
+++ express/server.js
@@ -7,11 +7,6 @@ const bodyParser = require('body-parser');
 var indexRouter = require('./routes/index');
 var resumesRouter = require('./routes/resume');
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
 app.use(bodyParser.json());
 
 app.use('/.netlify/functions/server', indexRouter, resumesRouter);  // path must route to lambda
