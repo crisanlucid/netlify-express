2d95915f0067c2bc5dc336fa007036a89d3c8009
crisanlucid <crisan.lucid@yahoo.com>
2019-05-16 14:07:41 +0200

    add new route

--- express/server.js
+++ express/server.js
@@ -10,8 +10,7 @@ router.get('/', (req, res) => {
   res.write('<h1>Hello from Express.js!</h1>');
   res.end();
 });
-router.get('/another', (req, res) => res.json({route: req.originalUrl}));
-router.post('/', (req, res) => res.json({postBody: req.body}));
+router.get('/api/v1/resume', (req, res) => res.json({route: req.originalUrl}));
 
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router); // path must route to lambda
