8084d16691f38994dad68607f669c0c2acd2057a
crisanlucid <crisan.lucid@yahoo.com>
2019-05-16 12:56:01 +0200

    add router from files

--- express/server.js
+++ express/server.js
@@ -4,17 +4,17 @@ const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
 
-const router = express.Router();
-router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
-  res.end();
+var indexRouter = require('./routes/index');
+var resumesRouter = require('./routes/resume');
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
-router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-router.post('/', (req, res) => res.json({ postBody: req.body }));
 
 app.use(bodyParser.json());
-app.use('/.netlify/functions/server', router);  // path must route to lambda
+
+app.use('/.netlify/functions/server', indexRouter, resumesRouter);  // path must route to lambda
 
 module.exports = app;
 module.exports.handler = serverless(app);
